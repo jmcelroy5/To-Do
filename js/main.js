a4a43c9f4c2c1567ab66860a6b65b1c32e6bdf7d
Jessica McElroy <mcelroyjessica@gmail.com>
2015-01-19 22:42:40 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,162 @@
+var List = {
+	items: [],
+	numLeft: 0,
+	numCompleted: 0,
+	addItem: function(text){
+		var toDo = {
+			id: this.getNextNum(),
+			text: text,
+			complete: false
+		};
+		createItemView(toDo);
+		this.items.push(toDo);
+		this.updateCounters();
+	},
+	deleteItem: function(id){
+		var idx = this.getItemIdx(id);
+		this.items.splice(idx, 1);
+		this.updateCounters();
+	},
+	toggleStatus: function(id){
+		var idx = this.getItemIdx(id);
+		var item = this.items[idx];
+		this.items[idx].complete = !this.items[idx].complete;
+		this.updateCounters();
+	},
+	getItemIdx: function(id){
+		for (var i=0; i < this.items.length; i++){
+			if (this.items[i].id == id) {
+				return i;
+			}
+		}
+	},
+	getRemaining: function(){
+		return this.items.filter(function(item){
+			return !item.complete;
+		});
+	},
+	getCompleted: function(){
+		return this.items.filter(function(item){
+			return item.complete;
+		});
+	},
+	clearCompleted: function(){
+		for (var i = this.items.length - 1; i >= 0; i--){
+			if (this.items[i].complete){
+				this.items.splice(i,1);
+			}
+		}
+	},
+	updateCounters: function(){
+		this.numLeft = this.getRemaining().length;
+		this.numCompleted = this.getCompleted().length;
+		$("#num-left").text(this.numLeft + " items left");
+		$("#num-complete").text("(" + this.numCompleted + ")");
+		return this.numLeft;
+	}
+};
+
+// utility function to generate unique ID
+(function(){
+		this.counter = 0;
+		this.getNextNum = function(){
+			this.counter += 1;
+			return this.counter;
+		};
+}).apply(List);
+
+var createItemView = function(item){
+
+	var itemView = $('<div></div>', {
+		'class': 'todo',
+		text: item.text
+	});
+
+	var checkBox = $('<div></div>', {
+		'class': 'checkbox',
+	});
+
+	if (item.complete){
+		itemView.addClass("complete");
+		checkBox.toggleClass("fa fa-check");
+	}
+
+	checkBox.click(function(){
+		List.toggleStatus(item.id);
+		itemView.toggleClass("complete");
+		checkBox.toggleClass("fa fa-check");
+	});
+
+	var deleteButton = $('<span></span>', {
+		'class': 'delete',
+		text: 'X',
+	});
+
+	deleteButton.click(function(){
+		itemView.remove();
+		List.deleteItem(item.id);
+	});
+
+	itemView.prepend(checkBox);
+	itemView.append(deleteButton);
+
+	$('#items').append(itemView);
+};
+
+var addItemHandler = function(){
+	var text = $("#input-field").val();
+	if (text){
+		$("#input-field").val('');
+		List.addItem(text);
+	}
+};
+
+var filterListHandler = function(view){
+	switch (view){
+		case "all":
+			renderList(List.items);
+			break;
+		case "remaining":
+			renderList(List.getRemaining());
+			break;
+		case "completed":
+			renderList(List.getCompleted());
+			break;
+	}
+};
+
+var renderList = function(items){
+	$("#items").empty();
+	for (var i=0; i < items.length; i++){
+		createItemView(items[i]);
+	}
+};
+
+$(document).ready(function(){
+	// event listeners for adding new item
+	$("#add-button").click(addItemHandler);
+	$("#input-field").keypress(function(e){
+		if (e.which == 13){
+			addItemHandler();
+		}
+	});
+
+	// event listener on filters
+	$("#filter").on("click", "li", function(e){
+		var $selected = $(this);
+		var filter = $(this).attr("id");
+		$("#filter li").removeClass("active");
+		$selected.addClass("active");
+		filterListHandler(filter);
+	});
+
+	// event listener on 'clear' btn
+	$("#clear").on("click", function(e){
+		List.clearCompleted();
+		renderList(List.items);
+	});
+});
+
+// drag and drop to reorder
+// use local storage to persist
+
